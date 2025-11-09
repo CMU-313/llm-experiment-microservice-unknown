c841ab05bc7d26e6c460b0b9d1e90b40a96c62ca
Alex Cheung <alexjakhangcheung@gmail.com>
2025-11-09 23:07:42 +0000

    feat : Copied over code from colab for get_translation and get_language

--- src/translator.py
+++ src/translator.py
@@ -1,3 +1,36 @@
+import os
+from ollama import Client
+
+MODEL_NAME = "mistral:7b"
+
+OLLAMA_URL = os.getenv("OLLAMA_HOST", "localhost:11434")
+client = Client(host=OLLAMA_URL)
+
+TRANSLATION_CONTEXT = """\
+    You are a language translator.  Translate the ENTIRE input from user into english and return ONLY that
+
+    The following are examples of expected input and output.
+
+    Examples:
+    INPUT: Bonjour, je m'appelle Bob
+    OUTPUT: Hello, my name is Bob.
+
+    INPUT: Können Sie mir bitte helfen?
+    OUTPUT: Can you please help me?
+    """
+
+CLASSIFICATION_CONTEXT = """\
+    You are a language classifier. Detect the language of the given text in user's content and reply with one word ONLY the English name of that language.
+
+    Example:
+    INPUT: Bonjour, je m'appelle Bob
+    OUTPUT: French
+
+    INPUT: Können Sie mir bitte helfen?
+    OUTPUT: German
+    """
+
+
 def translate_content(content: str) -> tuple[bool, str]:
     try:
         translation = get_translation(content)
@@ -22,9 +55,41 @@ def translate_content(content: str) -> tuple[bool, str]:
         return (False, "An unexpected error occurred while processing your request.")
 
 
-def get_language(content: str) -> str:
-    pass
+def get_language(post: str) -> str:
+    context = CLASSIFICATION_CONTEXT
+    response = client.chat(
+    model=MODEL_NAME,
+    messages=[
+        {
+            "role": "system",
+            "content": context
+        },
+        {
+            "role": "user",
+            "content": post
+        }
+    ]
+    )
+    if MODEL_NAME=="mistral:7b":
+      return response.message.content[1:]
+    return response.message.content
 
 
-def get_translation(content: str) -> str:
-    pass
+def get_translation(post: str) -> str:
+    context = TRANSLATION_CONTEXT
+    response = client.chat(
+    model=MODEL_NAME,  # model name
+    messages=[
+        {
+            "role": "system",
+            "content": context
+        },
+        {
+            "role": "user",
+            "content": post
+        }
+    ]
+    )
+    if MODEL_NAME=="mistral:7b":
+      return response.message.content[1:]
+    return response.message.content
