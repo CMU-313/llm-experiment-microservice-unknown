775583b98fc390cb8764ae5d5a7363adffa077f1
Hyeonjae Park <hyeonjaep@Hyeonjaes-MacBook-Pro.local>
2025-11-11 12:15:09 -0500

    fix : chagne translation logic for detection first

--- src/translator.py
+++ src/translator.py
@@ -32,21 +32,24 @@ CLASSIFICATION_CONTEXT = """\
 
 def translate_content(content: str) -> tuple[bool, str]:
     try:
-        translation = get_translation(content)
-        language = get_language(content)
+        # no need to translate if detected as english
+        language = get_language(content) 
+        if (not (isinstance(language, str))):
+            return (False, "There was an error detecting the language of your text.")
 
+        if language.lower().strip() == "english" :
+            return (True, content)
+        
+        translation = get_translation(content)
         # Basic checks for string output
         if (not (isinstance(translation, str))):
             return (False, "There was an error translating your text.")
 
-        if (not (isinstance(language, str))):
-            return (False, "There was an error detecting the language of your text.")
+        # is_english = False
+        # if language.lower().strip() == "english":
+        #     is_english = True
 
-        is_english = False
-        if language.lower().strip() == "english":
-            is_english = True
-
-        return (language.lower().strip(), translation)
+        return (False, translation)
 
     except Exception as e:
         # Catch any other unexpected errors during the LLM interaction
