ba4340c8eb6b48c68172515e2b313ef240b4832a
Alex Cheung <alexjakhangcheung@gmail.com>
2025-11-11 03:25:38 +0000

    testing : force using http

--- src/translator.py
+++ src/translator.py
@@ -3,7 +3,7 @@ from ollama import Client
 
 MODEL_NAME = "gemma3:1b"
 
-OLLAMA_URL = "localhost:11434"
+OLLAMA_URL = "http://localhost:11434"
 client = Client(host=OLLAMA_URL)
 
 TRANSLATION_CONTEXT = """\
